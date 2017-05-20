7bd4bd1db91413008e4437e7f008e1a1ffd188b5
Adrian Salamon <adr.salamon@gmail.com>
2017-05-20 15:43:46 +0200

    Got react list working

--- client/index.js
+++ client/index.js
@@ -1,4 +1,80 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import {
+  SortableContainer,
+  SortableElement,
+  arrayMove
+} from "react-sortable-hoc";
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+const listStyle = {
+  backgroundColor: "#f3f3f3",
+  border: "1px solid #efefef",
+  borderRadius: "3px"
+};
+
+const listItemStyle = {
+  backgroundColor: "#fff",
+  margin: "0",
+  listStyleType: "none",
+  padding: "12px 20px",
+  borderBottom: "1px solid #efefef"
+};
+
+const SortableItem = SortableElement(({ value, number }) => (
+  <li style={listItemStyle}>{number + 1}: {value}</li>
+));
+
+const SortableList = SortableContainer(({ items }) => {
+  return (
+    <ul style={listStyle}>
+      {items.map((value, index) => (
+        <SortableItem
+          key={`item-${index}`}
+          index={index}
+          value={value.name}
+          number={index}
+        />
+      ))}
+    </ul>
+  );
+});
+
+class SortableComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { name: "Socialdemokraterna", index: 0 },
+        { name: "Moderaterna", index: 1 },
+        { name: "Sverigedemokraterna", index: 2 },
+        { name: "Centerpartiet", index: 3 },
+        { name: "Vänsterpartiet", index: 4 },
+        { name: "Liberalerna", index: 5 },
+        { name: "Miljöpartiet", index: 6 },
+        { name: "Kristdemokraterna", index: 7 },
+        { name: "Feministisktinitiativ", index: 8 }
+      ]
+    };
+    this.onSortEnd = ({ oldIndex, newIndex }) => {
+      this.setState({
+        items: arrayMove(this.state.items, oldIndex, newIndex)
+      });
+    };
+  }
+
+  handleClicks = () => {
+    let json = this.state.items.map(item => item.index);
+    console.log(json);
+  };
+
+  render() {
+    return (
+      <div>
+        <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />
+        <button onClick={this.handleClicks} />
+      </div>
+    );
+  }
+}
+
+render(<SortableComponent />, document.getElementById("root"));
